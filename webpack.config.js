5edb61390f767e3df90a8398173e6c38a1d345f2
chendeyou <chendeyou@daojia-inc.com>
2019-12-18 22:29:08 +0800

    fix bug

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports={
     module: {
         rules: [
             {test:/\.js$/,use:'babel-loader',exclude:'/node_modules/'},
-            {test:/\.css$/,use:['style-loader,css-loader']},
+            {test:/\.css$/,use:['style-loader','css-loader']},
             {test:/\.less$/,use:['style-loader','css-loader','less-loader']},
             //转化base64只在8192字节以下转化，其他情况输出文件
             {test:/\.(jpg|png|gif)$/,use:'url-loader?limit=8192'}
